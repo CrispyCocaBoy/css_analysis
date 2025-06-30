c5a75031d61943f921bf636395a6004310f31d65
anthony-tricarico <tricarico672@gmail.com>
2025-06-30 12:13:47 +0200

    final review

--- R/figures_full_data.R
+++ R/figures_full_data.R
@@ -1,5 +1,5 @@
 library(pacman)
-p_load(tidyverse, ggplot2, ggthemes, scales, psych)
+p_load(tidyverse, ggplot2, ggthemes, scales, psych, lubridate, waffle, ggsci, Hmisc)
 
 df <- read_csv("results/aggregated/emotions/full_with_emotions_final.csv")
 
@@ -7,7 +7,7 @@ theme_set(theme_bw())
 
 # Plot 1 week from event --------------------------------------------------
 
-interval <- interval(as.Date("2018-02-14"), as.Date("2018-02-22"))
+interval <- lubridate::interval(as.Date("2018-02-14"), as.Date("2018-02-22"))
 
 first_wk <- df %>% 
   filter(created_utc %within% interval)
@@ -21,11 +21,13 @@ ggplot(aggregated_first_wk, aes(x = `date(created_utc)`, y = value, color = emot
   geom_line() +
   facet_grid(source ~ .) +
   labs(x = 'Date', y = 'Average Emotion Level', title = 'Emotion Level one Week from Event') +
-  theme(plot.title = element_text(hjust = .5))
+  theme(plot.title = element_text(hjust = .5)) +
+  scale_color_nejm()  
+
 
 # 1 Month from Event ------------------------------------------------------
 
-interval <- interval(as.Date("2018-02-14"), as.Date("2018-03-15"))
+interval <- lubridate::interval(as.Date("2018-02-14"), as.Date("2018-03-15"))
 
 first_wk <- df %>% 
   filter(created_utc %within% interval)
@@ -36,14 +38,17 @@ aggregated_first_wk <- first_wk %>%
   pivot_longer(!c(`date(created_utc)`, source), names_to = 'emotion', values_to = 'value')
 
 ggplot(aggregated_first_wk, aes(x = `date(created_utc)`, y = value, color = emotion)) +
-  geom_line() +
+  geom_line(linewidth = 1) +
   facet_grid(source ~ .) +
   labs(x = 'Date', y = 'Average Emotion Level', title = 'Emotion Level one Month from Event') +
-  theme(plot.title = element_text(hjust = .5))
+  scale_x_date(date_labels = "%b-%d", date_breaks = "5 days") +
+  theme(plot.title = element_text(hjust = .5)) +
+  scale_color_nejm()  
+
 
 # 1 Year from Event -------------------------------------------------------
 
-interval <- interval(as.Date("2018-02-14"), as.Date("2019-02-15"))
+interval <- lubridate::interval(as.Date("2018-02-14"), as.Date("2019-02-15"))
 
 first_wk <- df %>% 
   filter(created_utc %within% interval)
@@ -54,10 +59,12 @@ aggregated_first_wk <- first_wk %>%
   pivot_longer(!c(`date(created_utc)`, source), names_to = 'emotion', values_to = 'value')
 
 ggplot(aggregated_first_wk, aes(x = `date(created_utc)`, y = value, color = emotion)) +
-  geom_line() +
-  facet_grid(source ~ ., scales = 'free_y') +
+  geom_line(linewidth = 0.8, alpha = 0.7) +
+  facet_grid(source ~ ., scale = 'free_y') +
   labs(x = 'Date', y = 'Average Emotion Level', title = 'Emotion Level one Year from Event') +
-  theme(plot.title = element_text(hjust = .5))
+  scale_x_date(date_labels = "%b-%y", date_breaks = "1 month") +
+  theme(plot.title = element_text(hjust = .5)) +
+  scale_color_nejm()  
 
 # Overall Data ------------------------------------------------------------
 aggregated_first_wk <- df %>% 
@@ -75,8 +82,42 @@ ggplot(aggregated_first_wk, aes(x = `date(created_utc)`, y = value, color = emot
     plot.title = element_text(hjust = .5),
     axis.text.x = element_text(angle = 90, hjust = 1),
     strip.text = element_text(size = 12)
-  )
+  ) +
+  scale_color_nejm()  
 
+
+# Aggregate to get mean emotion per platform
+emotion_summary <- df %>%
+  group_by(source) %>%
+  dplyr::summarize(
+    dplyr::across(joy:anticipation, ~ mean(.x, na.rm = TRUE)),
+    .groups = "drop"
+  ) %>%
+  pivot_longer(-source, names_to = "emotion", values_to = "mean_value")
+
+# Barplot
+ggplot(emotion_summary, aes(x = reorder(emotion, -mean_value), y = mean_value, fill = emotion)) +
+  geom_col() +
+  facet_wrap(~ source) +
+  scale_fill_nejm() +
+  labs(
+    x = "Emotion",
+    y = "Average Level",
+    title = "Average Emotion Levels by Platform"
+  ) +
+  theme(
+    legend.position = "none",
+    plot.title = element_text(hjust = 0.5),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  ) +
+  scale_color_nejm()  
+
+ggplot(emotion_summary, aes(x = emotion, y = source, size = mean_value, color = emotion)) +
+  geom_point(alpha = 0.7) +
+  scale_size_area(max_size = 15) +
+  scale_color_nejm() +
+  labs(title = "Emotions Bubble Plot", x = "Emotion", y = "Platform") +
+  theme(legend.position = "right")
 
 # Descriptive Analysis ----------------------------------------------------
 
@@ -85,5 +126,8 @@ describeBy(df, 'source')
 # on YT that variability is almost matched by sadness
 
 GGally::ggpairs(df[which(df$source == "Reddit"), 6:ncol(df)], title = 'Emotions from Reddit (Entire Sample)')
-GGally::ggpairs(df[which(df$source == "YouTube"), 6:ncol(df)], title = 'Emotions from YouTube (Entire Sample)')
+print(GGally::ggpairs(df[which(df$source == "YouTube"), 6:ncol(df)], title = 'Emotions from YouTube (Entire Sample)'))
 
+cor(df[, 6:ncol(df)], use = 'complete.obs')
+
+rcorr(as.matrix(df[, 6:ncol(df)]), type = "spearman")
--- R/ts_modeling.R
+++ R/ts_modeling.R
@@ -1,6 +1,6 @@
 library(pacman)
 
-p_load(fpp3, ggplot2, ggthemes)
+p_load(fpp3, ggplot2, ggthemes, readr)
 
 df <- read_csv("results/aggregated/emotions/full_with_emotions_final.csv")
 
@@ -17,11 +17,6 @@ aggregated_ts <- aggregated_df %>%
 
 gg_season(aggregated_ts, joy)
 
-aggregated_ts %>% 
-  filter_index(. ~ "2021-12-31") %>% 
-  gg_season(sadness) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b")
-
 fit <- model(filter(aggregated_ts, source == "YouTube"), 
       'joy' = STL(joy ~ trend() + season(), robust = T),
       'trust' = STL(trust ~ trend() + season(), robust = T),
@@ -34,3 +29,43 @@ fit <- model(filter(aggregated_ts, source == "YouTube"),
       )
 
 autoplot(components(fit[2,]))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'sadness' = STL(sadness ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'joy' = STL(joy ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'trust' = STL(trust ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'fear' = STL(fear ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'surprise' = STL(surprise ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'disgust' = STL(disgust ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'anger' = STL(anger ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
+
+fit <- model(filter(aggregated_ts, source == "YouTube"), 
+             'anticipation' = STL(anticipation ~ trend() + season(), robust = T)
+)
+autoplot(components(fit))
